7268de2028e68bf671b212f20db7eb5bb2adddaf
Olga <olia_tarasenko@inbox.ru>
2023-03-29 13:08:42 +0300

    two commit

---
+++ Next/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string[] array =
+{
+    "hello",
+    "2",
+    "world",
+    ":-"       
+};
+ 
+var result = new string[array.Length];
+var realSize = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+ 
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
